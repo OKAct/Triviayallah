768169ee59973d84938b0759cbf84c6b2526a226
OKAct <anandyasheswi27@gmail.com>
2026-05-18 11:47:08 +0000

    please work 🙏🙏🙏

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [check,changeCheck]=useState("true");
 
 const get_ques= async ()=>{
-  const resp=await fetch("http://localhost:40329/easy")
+  const resp=await fetch("https://triviayallah-1.onrender.com")
 
   const ream=await resp.text();
 
